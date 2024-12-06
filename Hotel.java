474e8c4e9c2f7b0f123cf3359391899288c9981d
adiejhoi <adriennejhoi01@gmail.com>
2024-12-06 19:46:24 +0800

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -4,27 +4,44 @@ package FinalProject;
 public class Hotel {
     
    Hotel next; 
-   int RMNO;
-   String RMDESC;
-   String RMTYPE;
-   double PRICE;
-   String BOOKST; 
+   private final int RMNO;
+   private final String RMDESC;
+   private final String RMTYPE;
+   private final double PRICE;
+   private final String BOOKST; 
 
-  Hotel(int RMNO,String RMDESC, String RMTYPE, double PRICE, String BOOKST) {
+  public Hotel(int RMNO,String RMDESC, String RMTYPE, double PRICE, String BOOKST) {
     this.RMNO = RMNO;
     this. RMDESC = RMDESC;
     this.RMTYPE = RMTYPE;
     this.PRICE = PRICE;
     this.BOOKST = BOOKST;
     this.next = null;
-   }
+  }
+  
+  public int getRoomNum(){
+      return RMNO;
+  }
+  public String getDesc(){
+      return RMDESC;
+  }
+  public String getType(){
+      return RMTYPE;
+  }
+  public double getPrice(){
+      return PRICE;
+  }
+  public String getStat(){
+      return BOOKST;
+  }
+  
   void hotelDisplay(){
       System.out.println("=========================");
-      System.out.println("Room ID No.: " + RMNO);
-      System.out.println("Room Description: " + RMDESC);
-      System.out.println("Room Type: " + RMTYPE);
-      System.out.println("Price: " + PRICE);
-      System.out.println("Book Status: " + BOOKST);
+      System.out.println("Room ID No.: " + getRoomNum());
+      System.out.println("Room Description: " + getDesc());
+      System.out.println("Room Type: " + getType());
+      System.out.println("Price: " + getPrice());
+      System.out.println("Book Status: " + getStat());
       System.out.println("");
   }
 }
