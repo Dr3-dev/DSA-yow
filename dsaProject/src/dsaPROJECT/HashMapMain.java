d5f834b276f536cc1cebe22cd5eea55e8bdb678a
Dr3-dev <delfinandre.tp490@gmail.com>
2024-12-11 14:08:20 +0800

    Create HashMapMain.java

---
+++ dsaProject/src/dsaPROJECT/HashMapMain.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class HashMapMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, String> rooms = new HashMap<>();
+        HashMap<String, String> hashMapDemo = new HashMap<>();
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("-----Hash Function Menu-----");
+            System.out.println("[1] Hash Function");
+            System.out.println("[2] Hash Collision");
+            System.out.println("[3] Sample Applications");
+            System.out.println("[0] Exit");
+            System.out.print("Enter your choice: ");
+            String choice = sc.nextLine();
+
+            switch (choice) {
+                case "1": // Hash Function
+                    System.out.println("---- Hash Function ----");
+                    hashMapDemo.Hash_Function();
+                    break;
+
+                case "2": // Hash Collision
+                    System.out.println("---- Hash Collision ----");
+                    hashMapDemo.hashCollision();
+                    break;
+
+                case "3": // Sample Applications
+                    System.out.println("---- Sample Applications ----");
+                    hashMapDemo.sampleApplication();
+                    break;
+
+                case "0": // Exit
+                    System.out.println("Exiting the program. Goodbye!");
+                    isRunning = false;
+                    break;
+
+                default: // Invalid input
+                    System.out.println("Invalid choice! Please select a valid option.");
+                    break;
+            }
+        }
+
+        sc.close();
+    }
+}
